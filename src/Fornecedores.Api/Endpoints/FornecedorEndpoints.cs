c54f45d42750a4e5559a8bf7028dba64ca2e034b
Giovanny Santos <giovannysantos297@gmail.com>
2024-07-25 21:20:02 -0300

    Adicionar arquivos de projeto.

---
+++ src/Fornecedores.Api/Endpoints/FornecedorEndpoints.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using System.Net;
+
+namespace Fornecedores.Api.Endpoints
+{
+    public static class FornecedorEndpoints
+    {
+        public static RouteGroupBuilder MapFornecedorEndpoints(this RouteGroupBuilder builder)
+        {
+            builder.MapGet("/fornecedores", () => ObterFornecedores)
+                .Produces(200, typeof(List<string>))
+                .WithSummary("Obtém todos os fornecedores")
+                .WithOpenApi();
+
+            return builder;
+        }
+
+        public static List<string> ObterFornecedores(HttpContext context)
+        {
+            return ["Fornecedor 1", "Fornecedor 2"];
+        }
+    }
+}
