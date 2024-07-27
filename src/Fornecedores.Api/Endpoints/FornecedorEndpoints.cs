f7965364206baad75b499ed98b2b087daa41039d
Giovanny Santos <giovannysantos297@gmail.com>
2024-07-27 16:28:35 -0300

    Criação da estrutura inicial das Api's Mínimas

--- src/Fornecedores.Api/Endpoints/FornecedorEndpoints.cs
+++ src/Fornecedores.Api/Endpoints/FornecedorEndpoints.cs
@@ -1,23 +1,57 @@
-﻿using Microsoft.AspNetCore.Mvc.ApiExplorer;
-using System.Net;
-
-namespace Fornecedores.Api.Endpoints
+﻿namespace Fornecedores.Api.Endpoints
 {
     public static class FornecedorEndpoints
     {
         public static RouteGroupBuilder MapFornecedorEndpoints(this RouteGroupBuilder builder)
         {
-            builder.MapGet("/fornecedores", () => ObterFornecedores)
+            builder.MapGet("/fornecedores", (HttpContext context) => ObterFornecedores(context))
                 .Produces(200, typeof(List<string>))
                 .WithSummary("Obtém todos os fornecedores")
+                .WithOpenApi();
+            
+            builder.MapGet("/fornecedores/{id}", (HttpContext context, string id) => ObterFornecedorPorId(context, id))
+                .Produces(200, typeof(List<string>))
+                .WithSummary("Obtém um fornecedor pelo ID")
+                .WithOpenApi();
+
+            builder.MapPost("/fornecedores", (HttpContext context, string novoFornecedor) => AdicionarFornecedor(context, novoFornecedor))
+                .WithSummary("Adiciona um novo fornecedor")
+                .WithOpenApi();
+
+            builder.MapPut("/fornecedores/{id}", (HttpContext context, string id) => AtualizarFornecedor(context, id))
+                .WithSummary("Atualiza um fornecedor existente por ID")
+                .WithOpenApi();
+
+            builder.MapPut("/fornecedores/{id}", (HttpContext context, string id) => RemoverFornecedor(context, id))
+                .WithSummary("Remove um fornecedor existente por ID")
                 .WithOpenApi();
 
             return builder;
         }
 
+        private static string RemoverFornecedor(HttpContext context, string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static string AtualizarFornecedor(HttpContext context, string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static string AdicionarFornecedor(HttpContext context, string novoFornecedor)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static string ObterFornecedorPorId(HttpContext context, string id)
+        {
+            throw new NotImplementedException();
+        }
+
         public static List<string> ObterFornecedores(HttpContext context)
         {
-            return ["Fornecedor 1", "Fornecedor 2"];
+            throw new NotImplementedException();
         }
     }
 }
